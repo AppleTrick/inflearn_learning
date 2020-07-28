5d42cf9c608fd578fe9cf7901964ead2aaea7cf1
AppleTrick <arrans9591@gmail.com>
2020-07-28 22:44:33 +0900

    learn web basic 1. 정적페이지 2. MVC 3. API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,51 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
+    // 정적 방
     @GetMapping("io")
     public String hello(Model model){
         model.addAttribute("data","spring");
         return "hi";
     }
+    //mvc 방식
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-templeate";
+    }
+    // 뒤에서 html을 변형시켜 조작시켜줌 -> html 새롭게 변
+
+    //api 방식형 ex1
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //"hello spring"
+    }
+    // 내용 그대로 보내진다 다른 문구 하나도 없이
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
